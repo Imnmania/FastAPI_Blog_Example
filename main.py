f556512337ab8e81f63b0fa4ec0456c03c147896
Imnmania <niloyb@live.com>
2021-05-10 04:57:40 +0600

    get method || path parameter || query parameter

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {"data": {"name": "Niloy"}}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f'{limit} published blog from db'}
+    else:
+        return {"data": f'{limit} blogs from db'}
+    
 
-@app.get('/about')
-def about():
-    return {"data": "About Page"}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished work'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    """fetch blog with id = id"""
+    return {"data": id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit: Optional[int] = 0):
+    # fetch comments with blog id = id
+    # return {'data': {'1', '2'}}
+    # return limit
+    return {'data': {'id': id, 'numList': {'1', '2'}}}
+
+
+
