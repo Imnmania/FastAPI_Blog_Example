6198abca3081795dcf0ca546c47bb71d31a89824
Imnmania <niloyb@live.com>
2021-05-10 06:15:53 +0600

    POST method

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+################################ GET #############################
 
 @app.get('/blog')
 def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
@@ -15,10 +18,12 @@ def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
 def unpublished():
     return {'data': 'all unpublished work'}
 
+
 @app.get('/blog/{id}')
 def show(id: int):
     """fetch blog with id = id"""
     return {"data": id}
+
 
 @app.get('/blog/{id}/comments')
 def comments(id: int, limit: Optional[int] = 0):
@@ -27,5 +32,16 @@ def comments(id: int, limit: Optional[int] = 0):
     # return limit
     return {'data': {'id': id, 'numList': {'1', '2'}}}
 
+################################ POST #############################
 
+class Blog(BaseModel):
+    title: str
+    body: str 
+    published: Optional[bool] = False
+
+
+
+@app.post('/blog')
+def create_blog(body: Blog):
+    return {'data': f'blog is created with title as {body.title}'}
 
